04eaa612e199f9063c9efbb49f15e614e9aa4843
Hongtao Huang <hhuang@godaddy.com>
2021-09-15 22:54:19 -0700

    auth complete

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,60 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Amplify, { Auth, Storage } from 'aws-amplify';
+import { withAuth } from './lib/WithAuth';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+Amplify.configure({
+  Auth: {
+    identityPoolId: 'us-west-2:6be02c25-8ed1-41c3-be24-91fd359ae1ae', //REQUIRED - Amazon Cognito Identity Pool ID
+    region: 'us-west-2', // REQUIRED - Amazon Cognito Region
+    userPoolId: 'us-west-2_igXUlSWIY', //OPTIONAL - Amazon Cognito User Pool ID
+    userPoolWebClientId: '353nnho6ujdajmsko93qqftgr9', //OPTIONAL - Amazon Cognito Web Client ID
+  },
+  Storage: {
+    customPrefix: {
+      public: '',
+    },
+    AWSS3: {
+      bucket: 'naraku', //REQUIRED -  Amazon S3 bucket name
+      region: 'us-west-2', //OPTIONAL -  Amazon service region
+    }
+  }
+});
+
+class App extends React.Component<any, any> {
+
+  componentDidMount() {
+    Storage.list('') // for listing ALL files without prefix, pass '' instead
+        .then(result => console.log(result))
+        .catch(err => console.log(err));
+    // Storage.get("")
+    //     .then(result => console.log(result))
+    //     .catch(err => console.log(err));
+
+  }
+
+  render() {
+    return (
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo"/>
+            <p>
+              Edit <code>src/App.tsx</code> and save to reload.
+            </p>
+            <a
+                className="App-link"
+                href="https://reactjs.org"
+                target="_blank"
+                rel="noopener noreferrer"
+            >
+              Learn React
+            </a>
+          </header>
+        </div>
+    );
+  }
 }
 
-export default App;
+export default withAuth(App);
+
---
+++ src/lib/WithAuth.tsx
@@ -0,0 +1,65 @@
+import React, {
+    ComponentType,
+    FunctionComponent,
+} from 'react';
+import { Auth, appendToCognitoUserAgent } from '@aws-amplify/auth';
+import { AmplifyContainer, AmplifyAuthenticator, AmplifyAuthContainer, AmplifySignIn, AmplifySignOut } from '@aws-amplify/ui-react';
+import { onAuthUIStateChange, AuthState } from '@aws-amplify/ui-components';
+import { Logger } from '@aws-amplify/core';
+
+const logger = new Logger('withAuth');
+
+export function withAuth<T extends object>(Component: ComponentType<T>) {
+    const AppWithAuthenticator: FunctionComponent<T> = props => {
+        const [signedIn, setSignedIn] = React.useState(false);
+
+        React.useEffect(() => {
+            appendToCognitoUserAgent('withAuth');
+
+            // checkUser returns an "unsubscribe" function to stop side-effects
+            return checkUser();
+        });
+
+        function checkUser() {
+            setUser();
+
+            return onAuthUIStateChange(authState => {
+                if (authState === AuthState.SignedIn) {
+                    setSignedIn(true);
+                } else if (authState === AuthState.SignedOut) {
+                    setSignedIn(false);
+                }
+            });
+        }
+
+        async function setUser() {
+            try {
+                const user = await Auth.currentAuthenticatedUser();
+                if (user) setSignedIn(true);
+            } catch (err) {
+                logger.debug(err);
+            }
+        };
+
+        if (!signedIn) {
+            return (
+                <AmplifyContainer>
+                    <AmplifyAuthContainer>
+                        <AmplifyAuthenticator  {...props}>
+                            <AmplifySignIn slot="sign-in" hideSignUp />
+                        </AmplifyAuthenticator>
+                    </AmplifyAuthContainer>
+                </AmplifyContainer>
+            );
+        }
+
+        return (
+            <>
+                <AmplifySignOut />
+                <Component {...props} />
+            </>
+        );
+    };
+
+    return AppWithAuthenticator;
+}
