8cdb08350f8fc6bb3c1f1addb0ea01e786bca83f
Hongtao Huang <hhuang@godaddy.com>
2021-09-19 13:26:09 -0700

    Updates

---
+++ static/js/17.0bba28d8.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpnaraku=this.webpackJsonpnaraku||[]).push([[17],{201:function(t,n,e){"use strict";e.r(n),e.d(n,"amplify_auth_container",(function(){return u}));var i=e(9),u=function(){function t(t){Object(i.k)(this,t)}return t.prototype.render=function(){return Object(i.i)(i.b,null,Object(i.i)("form",{autoComplete:"on",hidden:!0},Object(i.i)("input",{name:"username"}),Object(i.i)("input",{name:"password",type:"password"}),Object(i.i)("input",{type:"submit"})),Object(i.i)("slot",null))},t}()}}]);
+//# sourceMappingURL=17.0bba28d8.chunk.js.map
