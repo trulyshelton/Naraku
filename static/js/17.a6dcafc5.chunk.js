0965f2b0be018e5db16e2b8e524d9607ab06aecb
Shelton <trulyshelton@gmail.com>
2022-01-06 16:56:28 -0800

    Updates

---
+++ static/js/17.a6dcafc5.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpnaraku=this.webpackJsonpnaraku||[]).push([[17],{231:function(t,n,e){"use strict";e.r(n),e.d(n,"amplify_auth_container",(function(){return u}));var i=e(14),u=function(){function t(t){Object(i.k)(this,t)}return t.prototype.render=function(){return Object(i.i)(i.b,null,Object(i.i)("form",{autoComplete:"on",hidden:!0},Object(i.i)("input",{name:"username"}),Object(i.i)("input",{name:"password",type:"password"}),Object(i.i)("input",{type:"submit"})),Object(i.i)("slot",null))},t}()}}]);
+//# sourceMappingURL=17.a6dcafc5.chunk.js.map
